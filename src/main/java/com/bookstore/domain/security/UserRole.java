abe87df81d1e54e6bee5e01962bf16fe1b8705a2
Ridwan Arip <arief_09@ymail.com>
2018-03-14 17:48:09 +0700

    add source

---
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -0,0 +1,66 @@
+package com.bookstore.domain.security;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.bookstore.domain.User;
+
+@Entity
+@Table(name="user_role")
+public class UserRole implements Serializable {
+	private static final long serialVersionUID = 890345L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long userRoleId;
+	
+	public UserRole () {}
+	
+	public UserRole (User user, Role role) {
+		this.user = user;
+		this.role = role;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable=false)
+	private User user;
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "role_id", nullable=false)
+	private Role role;
+
+	public long getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(long userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+}
